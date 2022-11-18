d704294f09fcc24ffb51acebb7c7ec4d239f7479
Gabriel Tuzzo <76889705+GabrielTuzzo@users.noreply.github.com>
2022-11-18 19:30:48 +0000

    new commit

--- js/index.js
+++ js/index.js
@@ -5,3 +5,13 @@ dropMenu.addEventListener('click', () => {
     menuList.classList.toggle('active')
     dropMenu.classList.toggle('active-btn')
 })
+
+addEventListener("load", () => {
+    const loader =  document.querySelector(".loader")
+    
+    loader.classList.add("loader-hidden")
+
+    document.querySelector(".loader").addEventListener("transitionend", () => {
+        document.body.removeChild(document.querySelector(".loader"))
+    })
+})
